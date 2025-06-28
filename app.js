72ff64daf366bd58c8e8aa3e36d30c7ce3693b64
Eren Celik <erencelik2426@gmail.com>
2025-06-28 13:07:02 +0300

    lastupdate 28.06.2025

--- app.js
+++ app.js
@@ -3,25 +3,54 @@ function toggleContent(headerElement) {
     content.style.display = (content.style.display === "block") ? "none" : "block";
 }
 
-function toggleSidebar() {
+document.addEventListener('DOMContentLoaded', () => {
   const sidebar = document.getElementById('sidebar');
-  const hamburger = document.getElementById('hamburger');
-  const main = document.getElementById('main-content');
+  const hamburger = document.querySelector('.hamburger');
+  const sagyazi = document.querySelector('.sagbutonyazi');
+  const solyazi = document.querySelector('.solbutonyazi');
 
-  sidebar.classList.toggle('collapsed');
-  
-  
-  if (sidebar.classList.contains('collapsed')) {
-    hamburger.style.right = 'auto';
-    hamburger.style.left = '10px';
-  } else {
-    hamburger.style.left = 'auto';
-    hamburger.style.right = '10px';
-  }
-}
+  window.toggleSidebar = function () {
+    sidebar.classList.toggle('collapsed');
+
+    if (sidebar.classList.contains('collapsed')) {
+      hamburger.style.right = 'auto';
+      hamburger.style.left = '10px';
+      sagyazi.style.fontSize = "large";
+      solyazi.style.fontSize = "large";
+    } else {
+      hamburger.style.left = 'auto';
+      hamburger.style.right = '10px';
+      sagyazi.style.fontSize = "initial";
+      solyazi.style.fontSize = "initial";
+    }
+  };
+});
+
 
 
 const toggleButon = document.getElementById('themeToggle');
 toggleButon.addEventListener('click',()=>{
   document.body.classList.toggle('white-mode');
 })
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const videoLinks = document.querySelectorAll('.video-line a[data-video]');
+  const iframe = document.querySelector('.iframe-video');
+
+  videoLinks.forEach(link => {
+    link.addEventListener('click', function (e) {
+      e.preventDefault();
+
+    
+      const videoUrl = this.getAttribute('data-video');
+      iframe.src = videoUrl;
+
+      
+      videoLinks.forEach(l => l.classList.remove('active-video')); // önceki aktiflerin hepsini siliyorum
+
+      
+      this.classList.add('active-video'); // for each dongusunde sıra hangısınde ıse active video o oluyor
+    });
+  });
+});
